bb3d1af93631d622d5861d5324047ab45ff3d4fd
austinv11 <austinv11@gmail.com>
2016-04-16 13:16:44 -0400

    Added a command to switch the bot's prefix

--- src/main/kotlin/kotbot/modules/impl/UtilityModule.kt
+++ src/main/kotlin/kotbot/modules/impl/UtilityModule.kt
@@ -1,9 +1,7 @@
 package kotbot.modules.impl
 
-import kotbot.modules.BaseModule
-import kotbot.modules.Command
-import kotbot.modules.CommandPermissionLevels
-import kotbot.modules.Parameter
+import kotbot.KotBot
+import kotbot.modules.*
 import sx.blah.discord.api.IListener
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent
 import sx.blah.discord.handle.obj.IMessage
@@ -18,36 +16,49 @@ class UtilityModule : BaseModule() {
     override fun enableModule(): Boolean {
         registerCommands(object: Command("cli", arrayOf(), "Mirrors the bot's OS's CLI.", arrayOf(Parameter("arguments", true)),
                 CommandPermissionLevels.OWNER, async = true) {
-            override fun execute(message: IMessage, args: List<Any>): String? {//FIXME: Some weird things happen in the cli
-                message.channel.sendMessage("Entering CLI mode, any messages ${message.author.mention()} sends in this " +
-                        "channel will be sent to this process until it terminates.")
-                val command = message.content.split(" ").toMutableList()
-                command.removeAt(0)
-                val process = ProcessBuilder(command)
-                        .redirectOutput(ProcessBuilder.Redirect.PIPE)
-                        .redirectErrorStream(true)
-                        .redirectInput(ProcessBuilder.Redirect.PIPE)
-                        .start()
-                val monitor = thread(isDaemon = true, block = { //InputStream monitor
-                    val scanner = Scanner(process.inputStream)
-                    while (process.isAlive) {
-                        if (scanner.hasNextLine()) {
-                            message.channel.sendMessage("`${scanner.nextLine()}`")
-                        }
+                            override fun execute(message: IMessage, args: List<Any>): String? {//FIXME: Some weird things happen in the cli
+                                message.channel.sendMessage("Entering CLI mode, any messages ${message.author.mention()} sends in this " +
+                                        "channel will be sent to this process until it terminates.")
+                                val command = message.content.split(" ").toMutableList()
+                                command.removeAt(0)
+                                val process = ProcessBuilder(command)
+                                        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                                        .redirectErrorStream(true)
+                                        .redirectInput(ProcessBuilder.Redirect.PIPE)
+                                        .start()
+                                val monitor = thread(isDaemon = true, block = { //InputStream monitor
+                                    val scanner = Scanner(process.inputStream)
+                                    while (process.isAlive) {
+                                        if (scanner.hasNextLine()) {
+                                            message.channel.sendMessage("`${scanner.nextLine()}`")
+                                        }
+                                    }
+                                })
+                                val channelCLI = IListener<MessageReceivedEvent> {
+                                    if (it.message.channel == message.channel && it.message.author == message.author) {
+                                        process.outputStream.bufferedWriter().appendln(it.message.content)
+                                    }
+                                }
+                                client.dispatcher.registerListener(channelCLI)
+                                process.waitFor()
+                                client.dispatcher.unregisterListener(channelCLI)
+                                return "CLI mode ended."
+                            }
+                
+                        },
+                object: Command("prefix", arrayOf(), "Switches the bot's active prefix.", arrayOf(Parameter("new prefix")),
+                        CommandPermissionLevels.ADMIN) {
+                    override fun execute(message: IMessage, args: List<Any>): String? {
+                        if (args.size < 1)
+                            throw CommandException("Need a new prefix to swtich to!")
+                        
+                        KotBot.CONFIG.PREFIX = args[0].toString()
+                        KotBot.CONFIG.save()
+                        
+                        return "KotBot's prefix changed to `${KotBot.CONFIG.PREFIX}`"
                     }
-                })
-                val channelCLI = IListener<MessageReceivedEvent> {
-                    if (it.message.channel == message.channel && it.message.author == message.author) {
-                        process.outputStream.bufferedWriter().appendln(it.message.content)
-                    }
-                }
-                client.dispatcher.registerListener(channelCLI)
-                process.waitFor()
-                client.dispatcher.unregisterListener(channelCLI)
-                return "CLI mode ended."
-            }
 
-        })
+                })
         return true
     }
 
